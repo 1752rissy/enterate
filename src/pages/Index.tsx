3ec8b40befd14318b4999e4b530bc4313670b12d
Andrés Gutierrez <andres.gutierrez@externos-cl.cencosud.com>
2025-09-15 17:50:17 -0300

    El modal ahora usa un ancho dinámico (w-[90vw] en móviles y sm:w-[600px] en desktop), centrado y con padding, para asegurar que se renderice correctamente y no quede angosto.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -510,7 +510,7 @@ const Index: React.FC = () => {
 
             {/* Formulario de creación de evento como modal Dialog (igual que moderador) */}
             <Dialog open={showCreateEventForm} onOpenChange={setShowCreateEventForm}>
-              <DialogContent className="w-full max-w-md sm:max-w-xl mx-auto max-h-[90vh] overflow-y-auto p-6 rounded-2xl shadow-xl flex flex-col justify-center items-center">
+              <DialogContent className="max-w-full w-[90vw] sm:w-[600px] mx-auto max-h-[90vh] overflow-y-auto p-6 rounded-2xl shadow-xl">
                 <DialogHeader>
                   <DialogTitle>Crear Nuevo Evento</DialogTitle>
                 </DialogHeader>
